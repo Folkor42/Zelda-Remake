7130508c1c766ac3b1f4c151ef1d24841d2995f8
James Nye <folkor@gmail.com>
2024-11-20 21:31:50 -0500

    Worked on a fixed version of Link

--- Scenes/link.gd
+++ Scenes/link.gd
@@ -3,12 +3,14 @@ extends CharacterBody2D
 @export var speed : int = 120
 @onready var animation_player = $AnimationPlayer
 @onready var sprite = $Sprite2D
+@onready var sword: Sprite2D = $Sword
 
 var cardinal_direction : Vector2 = Vector2.DOWN
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
+	sword.scale.x = 1
 	sprite.scale.x = 1
 	if input_direction==Vector2(0, 1):
 		animation_player.play("walk_down")
@@ -18,6 +20,7 @@ func get_input():
 		cardinal_direction = Vector2.UP
 	elif input_direction==Vector2(-1, 0):
 		sprite.scale.x = -1
+		sword.scale.x = -1
 		animation_player.play("walk_side")
 		cardinal_direction = Vector2.LEFT
 	elif input_direction==Vector2(1, 0):
@@ -25,15 +28,30 @@ func get_input():
 		cardinal_direction = Vector2.RIGHT
 	else:
 		if cardinal_direction==Vector2.UP:
-			animation_player.play("idle_up")
+			if Input.is_action_pressed("attack"):
+				animation_player.play("attack_up")
+			else:
+				animation_player.play("idle_up")
 		elif cardinal_direction==Vector2.DOWN:
-			animation_player.play("idle_down")
+			if Input.is_action_pressed("attack"):
+				animation_player.play("attack_down")
+			else:
+				animation_player.play("idle_down")
 		elif cardinal_direction==Vector2.RIGHT:
-			animation_player.play("idle_side")
 			sprite.scale.x = 1
+			if Input.is_action_pressed("attack"):
+				animation_player.play("attack_right")
+			else:
+				animation_player.play("idle_side")
 		elif cardinal_direction==Vector2.LEFT:
 			sprite.scale.x = -1
-			animation_player.play("idle_side")
+			sword.scale.x = -1
+			if Input.is_action_pressed("attack"):
+				animation_player.play("attack_left")
+				await animation_player.animation_finished
+			else:
+				animation_player.play("idle_side")
+
 
 func _physics_process(_delta):
 	get_input()
